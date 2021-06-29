ab4b059539c34dc2e06b17375b9cdaa804087947
valerii rzhevskii <rvs.19@uni-dubna.ru>
2021-06-29 23:35:01 +0300

    test commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace AutoInitio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(826, 393);
             this.Name = "Form1";
             this.Text = "AutoInition";
             this.ResumeLayout(false);
