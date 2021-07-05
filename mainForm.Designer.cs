d85f580319a85df8578f1820543cb89c23a6065b
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2021-07-06 00:45:53 +0300

    Task scheduler added
    Some bugs with array of tasks

--- mainForm.Designer.cs
+++ mainForm.Designer.cs
@@ -37,6 +37,9 @@ namespace AutoInitio
             this.buttonRegistry = new System.Windows.Forms.Button();
             this.panelMain = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonFolderOpen = new System.Windows.Forms.Button();
+            this.buttonSchedulerOpen = new System.Windows.Forms.Button();
+            this.buttonRegistryOpen = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // notifyIconTray
@@ -51,7 +54,7 @@ namespace AutoInitio
             // 
             // buttonFolder
             // 
-            this.buttonFolder.Location = new System.Drawing.Point(12, 100);
+            this.buttonFolder.Location = new System.Drawing.Point(12, 12);
             this.buttonFolder.Name = "buttonFolder";
             this.buttonFolder.Size = new System.Drawing.Size(115, 50);
             this.buttonFolder.TabIndex = 1;
@@ -61,7 +64,7 @@ namespace AutoInitio
             // 
             // buttonScheduler
             // 
-            this.buttonScheduler.Location = new System.Drawing.Point(12, 175);
+            this.buttonScheduler.Location = new System.Drawing.Point(12, 125);
             this.buttonScheduler.Name = "buttonScheduler";
             this.buttonScheduler.Size = new System.Drawing.Size(115, 50);
             this.buttonScheduler.TabIndex = 2;
@@ -71,7 +74,7 @@ namespace AutoInitio
             // 
             // buttonRegistry
             // 
-            this.buttonRegistry.Location = new System.Drawing.Point(12, 250);
+            this.buttonRegistry.Location = new System.Drawing.Point(12, 237);
             this.buttonRegistry.Name = "buttonRegistry";
             this.buttonRegistry.Size = new System.Drawing.Size(115, 50);
             this.buttonRegistry.TabIndex = 3;
@@ -96,11 +99,44 @@ namespace AutoInitio
             this.textBox1.Size = new System.Drawing.Size(822, 232);
             this.textBox1.TabIndex = 0;
             // 
+            // buttonFolderOpen
+            // 
+            this.buttonFolderOpen.Location = new System.Drawing.Point(12, 69);
+            this.buttonFolderOpen.Name = "buttonFolderOpen";
+            this.buttonFolderOpen.Size = new System.Drawing.Size(115, 30);
+            this.buttonFolderOpen.TabIndex = 5;
+            this.buttonFolderOpen.Text = "Открыть";
+            this.buttonFolderOpen.UseVisualStyleBackColor = true;
+            this.buttonFolderOpen.Click += new System.EventHandler(this.buttonFolderOpen_Click);
+            // 
+            // buttonSchedulerOpen
+            // 
+            this.buttonSchedulerOpen.Location = new System.Drawing.Point(12, 181);
+            this.buttonSchedulerOpen.Name = "buttonSchedulerOpen";
+            this.buttonSchedulerOpen.Size = new System.Drawing.Size(115, 30);
+            this.buttonSchedulerOpen.TabIndex = 6;
+            this.buttonSchedulerOpen.Text = "Открыть";
+            this.buttonSchedulerOpen.UseVisualStyleBackColor = true;
+            this.buttonSchedulerOpen.Click += new System.EventHandler(this.buttonSchedulerOpen_Click);
+            // 
+            // buttonRegistryOpen
+            // 
+            this.buttonRegistryOpen.Location = new System.Drawing.Point(12, 293);
+            this.buttonRegistryOpen.Name = "buttonRegistryOpen";
+            this.buttonRegistryOpen.Size = new System.Drawing.Size(115, 30);
+            this.buttonRegistryOpen.TabIndex = 7;
+            this.buttonRegistryOpen.Text = "Открыть";
+            this.buttonRegistryOpen.UseVisualStyleBackColor = true;
+            this.buttonRegistryOpen.Click += new System.EventHandler(this.buttonRegistryOpen_Click);
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(846, 643);
+            this.Controls.Add(this.buttonRegistryOpen);
+            this.Controls.Add(this.buttonSchedulerOpen);
+            this.Controls.Add(this.buttonFolderOpen);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.buttonRegistry);
@@ -125,6 +161,9 @@ namespace AutoInitio
         private System.Windows.Forms.Button buttonRegistry;
         private System.Windows.Forms.Panel panelMain;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonFolderOpen;
+        private System.Windows.Forms.Button buttonSchedulerOpen;
+        private System.Windows.Forms.Button buttonRegistryOpen;
     }
 }
 
--- mainForm.cs
+++ mainForm.cs
@@ -15,6 +15,7 @@ namespace AutoInitio
     public partial class mainForm : Form
     {
         private List<string> nodesFolders = new List<string>();
+        private List<string[]> nodesScheduler = new List<string[]>();
 
         public mainForm()
         {
@@ -26,7 +27,7 @@ namespace AutoInitio
             this.WindowState = FormWindowState.Normal;
         }
 
-        private void buttonScheduler_Click(object sender, EventArgs e) // ToolTip
+        private void buttonScheduler_Click(object sender, EventArgs e)
         {
             panelMain.Controls.Clear();
 
@@ -35,13 +36,66 @@ namespace AutoInitio
             process.StartInfo.Arguments = "/query /fo csv /nh";
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
             process.Start();
+
             StreamReader reader = process.StandardOutput;
+
             string[] output = reader.ReadToEnd().Split('\n');
+
+            // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             foreach (string str in output)
             {
-                textBox1.Text += str + '\n';
+                nodesScheduler.Add(str.Split(','));
+                /*if (str.Replace("\"", "").Split(',')[2] != "Disabled")
+                {
+                    nodesScheduler.Add(str.Replace("\"", "").Split(','));
+                }*/
             }
+            // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            if (output.Length != 0) // исправить ошибку, но какую?
+            {
+                for (int i = 0; i < nodesScheduler.Count - 1; i++)
+                {
+                    Label labelTaskName = new Label();
+                    labelTaskName.Location = new Point(10, 15 + (i * 30));
+                    labelTaskName.AutoSize = false;
+                    labelTaskName.BorderStyle = BorderStyle.None;
+                    labelTaskName.AutoEllipsis = true;
+                    labelTaskName.Text = Path.GetFileName(nodesScheduler[i][0]);
+                    panelMain.Controls.Add(labelTaskName);
+
+                    // проблема с выходом за границы
+                    /*Label labelTaskTime = new Label();
+                    labelTaskTime.Location = new Point(90, 15 + (i * 30));
+                    labelTaskTime.AutoSize = false;
+                    labelTaskTime.BorderStyle = BorderStyle.None;
+                    labelTaskTime.AutoEllipsis = true;
+                    labelTaskTime.Text = Path.GetFileName(nodesScheduler[i][1]);
+                    panelMain.Controls.Add(labelTaskTime);*/
+
+                    // можно сделать в обе стороны работу
+                    /*Label labelTaskStatus = new Label();
+                    labelTaskStatus.Location = new Point(10, 15 + (i * 30));
+                    labelTaskStatus.AutoSize = false;
+                    labelTaskStatus.BorderStyle = BorderStyle.None;
+                    labelTaskStatus.AutoEllipsis = true;
+                    labelTaskStatus.Text = Path.GetFileName(nodesScheduler[i][3]);
+                    panelMain.Controls.Add(labelTaskStatus);*/
+                }
+            }
+            else // невозможно, но вдруг 
+            {
+                Label label = new Label();
+                label.Location = new Point(0, 0);
+                label.Dock = DockStyle.Fill;
+                label.AutoSize = false;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                label.Text = "Задачи не запланированы";
+                panelMain.Controls.Add(label);
+            }
+
             process.WaitForExit();
         }
 
@@ -52,6 +106,8 @@ namespace AutoInitio
 
         private void buttonFolder_Click(object sender, EventArgs e)
         {
+            panelMain.Controls.Clear();
+
             string[] files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Startup));
 
             if ((files.Length == 1 && Path.GetFileName(files[0]) == "desktop.ini") || files.Length == 0)
@@ -86,8 +142,6 @@ namespace AutoInitio
                 var result = MessageBox.Show("Вы уверены, что хотите удалить " + Path.GetFileName(nodesFolders[button.index]) + " ?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (result == DialogResult.No)
                 {
-                    // cancel the closure of the form.
-                    //e.Cancel = true;
                     return;
                 }
                 File.Delete(nodesFolders[button.index]);
@@ -117,5 +171,21 @@ namespace AutoInitio
                 panelMain.Controls.Add(button);
             }
         }
+
+        private void buttonFolderOpen_Click(object sender, EventArgs e)
+        {
+            Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+        }
+
+        private void buttonSchedulerOpen_Click(object sender, EventArgs e)
+        {
+            Process.Start("taskschd.msc");
+        }
+
+        private void buttonRegistryOpen_Click(object sender, EventArgs e)
+        {
+            Process.Start("regedit.exe");
+        }
+
     }
 }
